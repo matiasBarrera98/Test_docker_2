1b69f9f0273f241a44b15a13ba22cf90e36595b2
matiasBarrera98 <71670052+matiasBarrera98@users.noreply.github.com>
2023-10-31 17:33:37 -0300

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,82 @@
+"""
+TODO:
+- Limpiar y arreglar notebook
+- Panel lateral con Información del modelo e información del set de datos
+- Generar Dashboard Power BI
+- Cambiar estilo colores de la página
+"""
+
+
+categorias_trabajo = [
+    "Ingrese opción",
+    "Medicina",
+    "Construcción",
+    "Trabajo de Oficina",
+    "Agricultura",
+    "Educación",
+    "Tecnología de la Información (TI)",
+    "Manufactura",
+    "Arte y Entretenimiento",
+    "Servicios de Alimentos y Bebidas",
+    "Ventas y Marketing",
+    "Transporte y Logística",
+    "Servicios de Limpieza",
+    "Servicios de Cuidado de la Salud",
+    "Gobierno y Servicios Públicos",
+    "Medios de Comunicación",
+    "Consultoría",
+    "Industria Farmacéutica",
+    "Energía y Recursos Naturales",
+    "Sector de Viajes y Turismo",
+    "Sector Inmobiliario",
+    "Sector de Moda y Textiles",
+    "Sector Deportivo y Recreación",
+    "Servicios de Belleza y Estética",
+    "Sector Automotriz",
+    "Industria Aviación",
+    "Industria Marítima y Naviera",
+    "Sector de Telecomunicaciones",
+    "Estudiante",
+    "Otro (a)"
+]
+
+regiones_chile = [
+    "Ingrese opción",
+    "Región de Arica y Parinacota",
+    "Región de Tarapacá",
+    "Región de Antofagasta",
+    "Región de Atacama",
+    "Región de Coquimbo",
+    "Región de Valparaíso",
+    "Región Metropolitana de Santiago",
+    "Región del Libertador General Bernardo O'Higgins",
+    "Región del Maule",
+    "Región de Ñuble",
+    "Región del Biobío",
+    "Región de La Araucanía",
+    "Región de Los Ríos",
+    "Región de Los Lagos",
+    "Región de Aysén del General Carlos Ibáñez del Campo",
+    "Región de Magallanes y de la Antártica Chilena",
+    "Otra"
+]
+
+enfermedades_cronicas_comunes = [
+    "Ingrese opción",
+    "No",
+    "Diabetes tipo 2",
+    "Hipertensión arterial (presión arterial alta)",
+    "Enfermedades cardíacas (como la enfermedad coronaria)",
+    "Enfermedades respiratorias crónicas (como la enfermedad pulmonar obstructiva crónica, EPOC)",
+    "Artritis reumatoide",
+    "Enfermedad de Alzheimer",
+    "Enfermedad de Parkinson",
+    "Asma",
+    "Enfermedad renal crónica",
+    "Enfermedad hepática crónica (como la hepatitis crónica)",
+    "Enfermedad inflamatoria intestinal (como la enfermedad de Crohn y la colitis ulcerosa)",
+    "Osteoporosis",
+    "Enfermedades autoinmunes (como el lupus eritematoso sistémico)",
+    "Trastornos de la tiroides (como el hipotiroidismo y el hipertiroidismo)",
+    "Otra"
+]
